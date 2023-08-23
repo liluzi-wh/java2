8a60a6e4b8d1f8d4dd07413a700d64af0d7aaccc
Rauf <totqwu@gmail.com>
2023-08-23 10:52:05 +0300

    java

---
+++ script.js
@@ -0,0 +1,6 @@
+
+var First = +prompt('Введите первое число')
+var Second = +prompt('Введите второе число')
+var Third = +prompt('Введите третье число')
+var Final = (First + Second + Third)
+console.log(Final / 2);
